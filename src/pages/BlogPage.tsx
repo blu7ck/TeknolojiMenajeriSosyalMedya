915ab511f564660f5c4df8a744c3252859a770dc
blu7ck <blu4ck@outlook.com>
2025-10-23 14:21:04 +0300

    Fix blog footer background: remove blue colors, use #151516

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -127,7 +127,7 @@ export default function BlogPage() {
         </div>
 
         {/* Fixed Newsletter Footer */}
-        <div className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 shadow-lg z-50">
+        <div className="fixed bottom-0 left-0 right-0 shadow-lg z-50" style={{ backgroundColor: "#151516" }}>
           <div className="container mx-auto px-4">
             <NewsletterForm isDark={isDark} />
           </div>
